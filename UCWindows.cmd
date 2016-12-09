f4c101e0625d0f43e7d9e34e3682b71da7daa5f5
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-12-09 08:22:34 -0800

    Initial Commit This probably works, will test by 12/10/16. If it's not changed, assume it works as intended or that I'm dead.

---
+++ UCWindows.cmd
@@ -0,0 +1,4 @@
+REM This is the Windows version of the DoTE User Copier
+@ECHO OFF
+del "C:\My Documents\Dungeon of the Endless\Users-Copy"
+xcopy "C:\My Documents\Dungeon of the Endless\Users" "c:\My Documents\Dungeon of the Endless\Users-Copy" /E /I /Y /C /R
