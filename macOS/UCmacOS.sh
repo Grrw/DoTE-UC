f3ab8ece6ffdc6b113f7690ae227330633204b47
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-12-09 09:06:16 -0800

    Mac Version Initial Commit, Tested and Working

---
+++ macOS/UCmacOS.sh
@@ -0,0 +1,3 @@
+# Mac Version of the DoTE User Copier
+rm -rf ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
+cp -r ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
