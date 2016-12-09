34e046fc26206c653af728fd22d23da00e5b4ac4
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-12-09 09:06:56 -0800

    Moved and Removed

--- UCWindows.cmd
+++
@@ -1,4 +0,0 @@
-REM This is the Windows version of the DoTE User Copier
-@ECHO OFF
-del "C:\My Documents\Dungeon of the Endless\Users-Copy"
-xcopy "C:\My Documents\Dungeon of the Endless\Users" "c:\My Documents\Dungeon of the Endless\Users-Copy" /E /I /Y /C /R
--- UCmacOS.sh
+++
@@ -1,3 +0,0 @@
-# Mac Version of the DoTE User Copier
-rm -rf ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
-cp -r ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
