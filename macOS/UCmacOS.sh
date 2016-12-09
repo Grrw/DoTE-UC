69de06fdf12bdb4d63efa4c90b69d76716f1f2ac
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-12-09 09:26:21 -0800

    Add options to copy Users or replace Users

--- macOS/UCmacOS.sh
+++ macOS/UCmacOS.sh
@@ -1,3 +1,14 @@
 # Mac Version of the DoTE User Copier
-rm -rf ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
-cp -r ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
+
+while true; do
+    read -p "y for replace Users, n for copy Users: " yn
+    case $yn in
+        [Yy]* ) rm -rf ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users
+                mv ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users
+                cp -r ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
+                read -p "Replaced! ctrl + C to Quit, Return to Continue. ";;
+        [Nn]* ) rm -rf ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
+                cp -r ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users ~/Library/Application\ Support/Dungeon\ of\ the\ Endless/Users-Copy
+                read -p "Copied! ctrl + C to Quit, Return to Continue. ";;
+    esac
+done
