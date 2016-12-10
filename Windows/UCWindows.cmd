815591c58f3a588e2bda7fa5f0dc081acb1ba036
Ben Y <Grrw@users.noreply.github.com>
2016-12-10 09:14:02 -0800

    use rmdir rather than del
    I got del to work, but it wouldn't delete the folder, only its contents

--- Windows/UCWindows.cmd
+++ Windows/UCWindows.cmd
@@ -1,5 +1,8 @@
-REM This is the Windows version of the DoTE User Copier
-REM Broken
-@ECHO OFF
-del "C:\My Documents\Dungeon of the Endless\Users-Copy"
-xcopy "C:\My Documents\Dungeon of the Endless\Users" "c:\My Documents\Dungeon of the Endless\Users-Copy" /E /I /Y /C /R
+REM This is the working Windows version of the DoTE User Copier
+
+
+rmdir C:\Users\%USERNAME%\DOCUME~1\DUNGEO~1\USERS-~1 /S /Q
+
+pause
+
+xcopy  /E /I /Y /C /R C:\Users\%USERNAME%\DOCUME~1\DUNGEO~1\users C:\Users\%USERNAME%\DOCUME~1\DUNGEO~1\Users-Copy
