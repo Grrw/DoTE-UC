866ad27f5d7bc4087bd78a8ff872092e5868b1c6
Ben Y <Grrw@users.noreply.github.com>
2016-12-09 22:21:09 -0800

    Does not work

--- Windows/UCWindows.cmd
+++ Windows/UCWindows.cmd
@@ -1,5 +1,5 @@
 REM This is the Windows version of the DoTE User Copier
-REM Broken, I don't understand how to write Batch
+REM Broken
 @ECHO OFF
 del "C:\My Documents\Dungeon of the Endless\Users-Copy"
 xcopy "C:\My Documents\Dungeon of the Endless\Users" "c:\My Documents\Dungeon of the Endless\Users-Copy" /E /I /Y /C /R
